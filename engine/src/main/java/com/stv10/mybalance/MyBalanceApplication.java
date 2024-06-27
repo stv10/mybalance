bbdc6b90670fa791c978dcd32ca6b4c4db31f42e
stv10 <santiagoinbox1@gmail>
2024-06-26 22:00:44 -0300

    some pre config

---
+++ engine/src/main/java/com/stv10/mybalance/MyBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.stv10.mybalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBalanceApplication.class, args);
+	}
+
+}
---
+++ engine/src/test/java/com/stv10/mybalance/MyBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stv10.mybalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
